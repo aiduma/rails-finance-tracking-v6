e1c6a0f5ab5ea0004efb4bab511413e4c5fce1d6
Alberto <aiduma08@gmail.com>
2022-02-11 11:12:12 -0700

    Setup UserStock resource

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
+
     #self, allow the method to be called without creating an instance of the class
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(
