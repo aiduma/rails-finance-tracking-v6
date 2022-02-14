df7e91359ad505485e65bb83dcd5291c42bbee23
Alberto <aiduma08@gmail.com>
2022-02-14 15:41:27 -0700

    Add JavaScript response to form submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,26 @@ class UsersController < ApplicationController
   def my_friends
     @my_friends = current_user.friends
   end
+
+  def search
+    if params[:user].present?
+      @user= params[:user]
+      if @user
+        respond_to do |format|
+          format.js { render partial: 'users/friends_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a friend name or email to search"
+          format.js { render partial: 'users/friends_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a user name or email to search"
+        format.js { render partial: 'users/friends_result' }
+      end
+    end
+  end
 end
 
